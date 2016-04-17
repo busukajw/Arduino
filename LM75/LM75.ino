f049efab9013cfa5a104b0ce9562e6ab5caac9c2
Aaron Walker <aaron.walker@booking.com>
2016-04-17 15:18:01 +0200

    breaking up logic into small functions

--- LM75/LM75.ino
+++ LM75/LM75.ino
@@ -5,47 +5,34 @@
 #define LM75_CONF 1
 #define LM75_HYST 3
 
-uint16_t tempdata(byte *a, byte *b)
+uint16_t _reg2Data(uint8_t *msb, uint8_t *lsb)
 {
-  float temp = 0;
+  // convert the two bytes and return a float
+  // remove the first 5 bits of the lsb as only the first 3 bits are used
   uint16_t reg_data = 0;
-  int bitcheck;
-  Serial.print("MSB in hex: ");
-  Serial.println(*a,HEX);
-  Serial.print("Non-Shifted in binary: ");
-  Serial.println(*b, BIN);
-  *b = *b >> 5; // shift the last 5 bits's off the byte
-  //Serial.print("shifted: ");
-  //Serial.println(*b, BIN);
-  // if the temperature is negative the fist bit of the MSB will be 1
-  bitcheck = *a & 0x80; // so create and apply mask 10000000:
-  if (bitcheck) { // 1 pass's bool test so is a negative number
-    Serial.println("Need to do 2s Complement");
-  }
-  else {
-        // *a * 8, 'cause first bit isn't 1, but 8, second 16 and so on, a value
-        // * 8 has the effect of shifting the number in binary 3 places to the left
-    reg_data = *a;
-    Serial.print("Just a: ");
-    Serial.println(reg_data);
-    reg_data = reg_data  << 3;
-    Serial.print("RegData: ");
-    Serial.println(reg_data, BIN);
-    reg_data = reg_data + *b;
-    Serial.print("All reg_data: ");
-    Serial.println(reg_data);
-    temp = reg_data * 0.125;
-    Serial.print("Shifting Temp: ");
-    Serial.println(temp);
-    temp = (*a * 8 +*b) *0.125;
-    Serial.print("Temp: " );
-    Serial.println(temp);
-  }
+  *lsb = *lsb >> 5;
+  reg_data = *msb << 3; //need to make space for the lsb
+  reg_data = reg_data + *lsb; 
   
-  return temp;
+  return reg_data;
   
 }
-void getLM75data(uint8_t *msb, uint8_t *lsb)
+
+float _convData2Temp(uint16_t regData, uint8_t *msb)
+{
+  uint16_t bitcheck = 0;
+  float temp = 0;
+  bitcheck = *msb & 0x80; // so create and apply mask 10000000:
+  if (bitcheck) { // 1 pass's bool test so is a negative number
+    Serial.println("Need to do 2s Complement");
+    //call 2's complement
+  }
+  else {
+    temp = regData * 0.125;
+  }
+  return temp;
+}
+void _getData(uint8_t *msb, uint8_t *lsb)
 {
   Wire.begin();
   Wire.beginTransmission(byte(LM75_ADDR));
@@ -55,19 +42,19 @@ void getLM75data(uint8_t *msb, uint8_t *lsb)
   *lsb = Wire.read();
 }
 
-void showLM75data()
- {
-  uint8_t lsb,msb  = 0;
-  uint16_t regdata = 0;
-  getLM75data(&msb, &lsb);
-  tempdata(&msb, &lsb);
- }
+float temp()
+{
+  uint8_t msb,lsb = 0;
+  _getData(&msb, &lsb);
+  return _convData2Temp(_reg2Data(&msb, &lsb),&msb);
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("LM75 test.");
   delay(500);
-  showLM75data();
+  Serial.println(temp());
   Serial.print("LM75 test ended....");
 }
 
