df2a400b3036f277e9c828b4df9488e1a718bf50
Aaron Walker <busukajw@gmail.com>
2017-03-04 15:34:14 +0000

    First attempt at reading the ADC.

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -6,9 +6,10 @@
  *  encoder0PinA to Pin 2
  *  encoder0PinB to Pin4
  */
-Encoder encoder(2,4);
+Encoder encoder(2,6);
 //set the SPI DAC SS pin
 const int dacSlaveSelectPin = 9;
+SPISettings mySettings;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,33 +24,58 @@ void setup() {
   digitalWrite(dacSlaveSelectPin, HIGH);
   //setup the SPI bus
   SPI.begin();
-  SPI.beginTransaction(SPISettings(4000000,MSBFIRST,SPI_MODE0));
 
 }
 
 void loop() {
   
   // put your main code here, to run repeatedly:
+  Serial.print("Encoder Position: ");
+  Serial.println(encoder.getPosition());
   setDac(encoder.getPosition());
-
+  Serial.println("End");
+  delay(200);
 }
+// function to read from the ADC
+float readAdc(int channel) {
+  byte primaryMask = B00001111;
+  mySettings = SPISettings(1000000, MSBFIRST, SPI_MODE0);
+  noInterrupts();
+  digitalWrite(adcChipSelectionPin, LOW);
+  SPI.beginTransaction(mySettings);
+  byte ersteConfByte = B00000110;
+  byte tweedeConfByte = channel<< 6;
+  SPI.transfer(ersteConfByte)
+  byte firstHalf = SPI.transfer(tweedeConfByte);
+  byte secondHalf = SPI.transfer(0x00);
+  digitalWrite(adcChipSelectionPin, HIGH);
+  interrupts();
+  SPI.endTransaction();
+  firstHalf &= primaryMask;
+  int adcNumber = (firstHalf << 8) | secondHalf;
+  float value = (adcNumber * 2.048) / 4096.000;
+  return value;
+}
+  
 // function to set the dac
 void setDac(int rEncoderNumber){
-  byte initConfbyte = 0b00110000;
+  mySettings = SPISettings(4000000, MSBFIRST, SPI_MODE0);
+  SPI.beginTransaction(mySettings);
+  byte initConfbyte = B00110000;
   /* shift the 12bit number 8bits to the right. So that the 1st 4 bits are now the last 4 bits
    * then OR the number with the mask the primary byte now has the config data as the first 4 bits
    * and the first 4 bits of the rotary encoder are now the last 4 bits of the primaryByte
    */
   byte primaryByte = (rEncoderNumber >> 8) | initConfbyte;
-  byte mask = 0b00001111;
+  byte mask = B00001111;
   byte secondaryByte =  rEncoderNumber & mask ;
   noInterrupts();  //disable interrupts so the rotorary encoder does not interrupt the SPI comms
   digitalWrite(dacSlaveSelectPin, LOW); // pull the DAC low to get ready to send some data
   SPI.transfer(primaryByte); //send the first byte
   SPI.transfer(secondaryByte); //send the second byte
   interrupts(); // turn interrupts back on
-  SPI.end();
   digitalWrite(dacSlaveSelectPin, HIGH);
+  SPI.endTransaction();
 }
 void doEncoder(){
   /* if PinA and pinB are both high or both low it is spinning forward (CW)
